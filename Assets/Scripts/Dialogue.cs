236c6b60d0ed347f06f07317ca14ea4ea1aa73e7
Thomas <therve38@gmail.com>
2020-03-21 14:10:22 +0100

    debut de dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject DialogueBox;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
