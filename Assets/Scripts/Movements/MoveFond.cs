60f6fe541df73b262baae67c06017b79f6a9ad32
Fx <fx73000@yahoo.fr>
2020-03-24 19:31:40 +0100

    Revert "Merge pull request #4 from ThomasHerve/Essai"
    This reverts commit 75d11ef0b05e777ac12ad32b41f5d26f4241447b.

---
+++ Assets/Scripts/Movements/MoveFond.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFond : Mover
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MouvementsManager.SetFond(this.gameObject);
+        speed = MouvementsManager.speedfond;
+    }
+
+}
