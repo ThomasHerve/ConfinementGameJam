ff1f64362ef692a34648bccc2f7e4266293891cd
Zarukin <benoitsonz@mail.com>
2020-03-28 16:21:53 +0100

    item gestion

---
+++ Assets/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool tetine, madelene, medoc;
+    public GameObject madeleneIMG, tetineIMG, medocIMG;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AddTetine()
+    {
+        tetine = true;
+        tetineIMG.SetActive(true);
+    }
+
+    public void AddMadeleine()
+    {
+        madelene = true;
+        madeleneIMG.SetActive(true);
+    }
+
+    public void AddMedoc()
+    {
+        medoc = true;
+        medocIMG.SetActive(true);
+    }
+
+    public void RemoveTetine()
+    {
+        tetine = false;
+        tetineIMG.SetActive(false);
+    }
+
+    public void RemoveMadeleine()
+    {
+        madelene = false;
+        madeleneIMG.SetActive(false);
+    }
+
+    public void RemoveMedoc()
+    {
+        medoc = false;
+        medocIMG.SetActive(false);
+    }
+
+
+
+}
+
---
+++ Assets/item.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    public Inventory inv;
+    public bool tetine, madeleine, medoc;
+    private bool Inside;
+    public GameObject press;
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            press.SetActive(true);
+            Inside = true;
+            Debug.Log("passe");
+        }
+    }
+
+    public void Update()
+    {
+        if (Inside && Input.GetKeyDown(KeyCode.E))
+        {
+            if (tetine)
+            {
+                inv.AddTetine();
+                Destroy(this.gameObject);
+            }
+            else if (madeleine)
+            {
+                inv.AddMadeleine();
+                Destroy(this.gameObject);
+            }
+            else if (medoc)
+            {
+                inv.AddMedoc();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            press.gameObject.SetActive(false);
+            Inside = false;
+            Debug.Log("passe222");
+        }
+
+    }
+
+
+}
